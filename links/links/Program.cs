90ffbb907af5b2bb042f683da719d01d4b587301
Ali <ali.mammadov@easlearn.com>
2023-09-22 18:58:10 +0400

    .

--- links/links/Program.cs
+++ links/links/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public delegate void Func(string str);
+
+public class MyClass
+{
+    public string MyString {  get; set; }
+
+    public MyClass(string str) => MyString = str;
+
+    public void Space(string str)
+    {
+        string result = string.Join("_", str.ToCharArray());
+        Console.WriteLine(result);
+    }
+
+    public void Reverse(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        string result = new string(charArray);
+        Console.WriteLine(result);
+    }
+}
+
+public class Run
+{
+    public void runFunc(Func func, string str)
+    {
+        func(str);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter string");
+        var str = Console.ReadLine();
+        MyClass cls = new MyClass(str);
+
+        Func spaceFunc = new Func(cls.Space);
+        Func reverseFunc = new Func(cls.Reverse);
+
+        Run run = new Run();
+
+        Console.WriteLine("Space():");
+        run.runFunc(spaceFunc, str);
+
+        Console.WriteLine("Reverse():");
+        run.runFunc(reverseFunc, str);
+    }
+}
